0bb8375060d9370a0949b782c5ae5c3770d46e09
ieattoomuchrice3@gmail.com <dth2113@columbia.edu>
2017-04-15 10:30:06 -0400

    debugged

--- refugdist.py
+++ refugdist.py
@@ -5,7 +5,7 @@ Created on Tue Apr 04 16:33:03 2017
 @author: dahoang
 """
 
-import geopy   as gpy
+#import geopy   as gpy
 import pandas  as pd
 import seaborn as sns
 
@@ -14,6 +14,7 @@ def haversine(lon1, lat1, lon2, lat2):
   Calculate the great circle distance between two points 
   on the earth (specified in decimal degrees)
   """
+  from math import radians, cos, sin, asin, sqrt
   # convert decimal degrees to radians 
   lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
   # haversine formula 
@@ -26,13 +27,13 @@ def haversine(lon1, lat1, lon2, lat2):
   dist = km/0.621371
   return dist
 
-df = pd.read_csv('')
+df = pd.read_csv('C:/Users/Dat Tien Hoang/Documents/GitHub/unhcr_distances/unhcr_popstats_export_resettlement_all_data.csv', skiprows=3)
 df = df.rename(index=str, columns={'Country / territory of asylum/residence':'Destination'})
 # make all the words uppercase for ease
 df['Destination'] = map(lambda x: x.upper(), df['Destination'])
 df['Origin']      = map(lambda x: x.upper(), df['Origin'])
 
-countries = pd.read_csv('')
+countries = pd.read_csv('C:\Users\Dat Tien Hoang\Documents\GitHub\unhcr_distances\countries.csv')
 #countries['Destination'] = countries['name']
 #countries['Origin']      = countries['name']
 #countries['Destination'] = map(lambda x: x.upper(), countries['Destination'])
@@ -44,11 +45,13 @@ countries['name'] = map(lambda x: x.upper(), countries['name'])
 # do some joins...left joins
 df = df.merge(countries, how='left', left_on='Destination', right_on='name', suffixes=['_UNHCR_d', '_countries_d'])
 df = df.rename(index=str, columns={'latitude':'latitude_d', 'longitude':'longitude_d'})
-df = df.merge(countries, , how='left', left_on='Origin', right_on='name', suffixes=['_UNHCR_o', '_countries_o'])
+df = df.merge(countries, how='left', left_on='Origin', right_on='name', suffixes=['_UNHCR_o', '_countries_o'])
 df = df.rename(index=str, columns={'latitude':'latitude_o', 'longitude':'longitude_o'})
 
 # next...calculate the distances between the countries!
-df['distance'] = haversine(longitude_d, latitude_d, longitude_o, latitude_o)
+df['distance'] = ''
+for i in range(len(df['distance'])):
+    df['distance'][i] = haversine(df['longitude_d'][i], df['latitude_d'][i], df['longitude_o'][i], df['latitude_o'][i])
 
 # make a violinplot, plotting by origin
-sns.violinplot(x='year', y='country', hue='dist', data=df, palette="Set3", bw=.2, cut=1, linewidth=1)
+sns.violinplot(x='Year', y='Origin', hue='distance', data=df, palette="Set3", bw=.2, cut=1, linewidth=1)
